5c5a92c538f0e1720c6316b33d65b49864fa1e74
Venkatesh Mitukula (Venkey) <venkateshmitukula@gmail.com>
2026-04-16 13:08:53 +0530

    Update app.module.ts

--- app.module.ts
+++ app.module.ts
@@ -1,1 +1,30 @@
 
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { HttpClientModule } from "@angular/common/http";
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { PaymentComponent } from './payment/payment.component';
+
+import { HighchartsChartModule } from 'highcharts-angular';
+import { RegisterComponent } from './register/register.component';
+@NgModule({
+  declarations: [
+    AppComponent,
+    PaymentComponent,
+    RegisterComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    AppRoutingModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    HighchartsChartModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
