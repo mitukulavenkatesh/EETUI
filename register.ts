052d3ded821a6f785f6bcd5add2ce3601b77d31e
Venkatesh Mitukula (Venkey) <venkateshmitukula@gmail.com>
2026-04-16 13:07:51 +0530

    Update register.ts

--- register.ts
+++ register.ts
@@ -1,1 +1,34 @@
 
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrl: './register.component.css'
+})
+export class RegisterComponent {
+  registerForm: FormGroup;
+
+
+  constructor(private fb: FormBuilder) {
+    this.registerForm = this.fb.group({
+      username: ['', Validators.required],
+      password: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      mobile: ['', [
+        Validators.required,
+        Validators.pattern('^[0-9]{10}$')
+      ]]
+    });
+  }
+
+  submit() {
+    if (this.registerForm.valid) {
+      console.log(this.registerForm.value);
+    } else {
+      this.registerForm.markAllAsTouched();
+    }
+  }
+
+}
