505fe7d92f554199dda208d13819221454bec070
Venkatesh Mitukula (Venkey) <venkateshmitukula@gmail.com>
2026-04-16 13:09:48 +0530

    Update app-routing.ts

--- app-routing.ts
+++ app-routing.ts
@@ -1,1 +1,24 @@
 
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { PaymentComponent } from './payment/payment.component';
+import { RegisterComponent } from './register/register.component';
+import { AppComponent } from './app.component';
+
+const routes: Routes = [
+
+  { path: 'pay', component: PaymentComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: '', component: AppComponent },
+  { path: '**', component: AppComponent }
+
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule {
+
+
+}
